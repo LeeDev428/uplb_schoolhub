064defc4a93971b73df80c9b71c7c2f923cea7fb
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-04 10:15:15 +0800

    Update create.tsx

--- resources/js/pages/registrar/documents/create.tsx
+++ resources/js/pages/registrar/documents/create.tsx
@@ -99,7 +99,7 @@ export default function CreateDocuments({ categories }: Props) {
         <RegistrarLayout>
             <Head title="Requirements Manager" />
 
-            <div className="space-y-6">
+            <div className="space-y-6 p-6">
                 {/* Header */}
                 <div className="flex items-center justify-between">
                     <div>
