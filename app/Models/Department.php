af795b68d266284504cf6a96bda17b4a419225c1
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-06 12:21:19 +0800

    Update Department.php

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'level',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function programs(): HasMany
+    {
+        return $this->hasMany(Program::class);
+    }
+
+    public function yearLevels(): HasMany
+    {
+        return $this->hasMany(YearLevel::class);
+    }
 }
