c72204d78fe78788e500e4bb5aef7dfa3f2b9189
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-11 00:00:35 +0800

    Update DepartmentSeeder.php

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -11,39 +11,45 @@ class DepartmentSeeder extends Seeder
     {
         $departments = [
             [
+                'code' => 'ELEM',
                 'name' => 'Elementary Department',
                 'description' => 'Elementary education (Grades 1-6)',
-                'level' => 'elementary',
+                'classification' => 'K-12',
                 'is_active' => true,
             ],
             [
+                'code' => 'JHS',
                 'name' => 'Junior High School Department',
                 'description' => 'Junior high school education (Grades 7-10)',
-                'level' => 'junior_high',
+                'classification' => 'K-12',
                 'is_active' => true,
             ],
             [
+                'code' => 'SHS',
                 'name' => 'Senior High School Department',
                 'description' => 'Senior high school education (Grades 11-12)',
-                'level' => 'senior_high',
+                'classification' => 'K-12',
                 'is_active' => true,
             ],
             [
+                'code' => 'CCS',
                 'name' => 'College of Computer Studies',
                 'description' => 'Computer Science, Information Technology, and related programs',
-                'level' => 'college',
+                'classification' => 'College',
                 'is_active' => true,
             ],
             [
+                'code' => 'CBA',
                 'name' => 'College of Business Administration',
                 'description' => 'Business, Accountancy, and Management programs',
-                'level' => 'college',
+                'classification' => 'College',
                 'is_active' => true,
             ],
             [
+                'code' => 'CAS',
                 'name' => 'College of Arts and Sciences',
                 'description' => 'Liberal arts and science programs',
-                'level' => 'college',
+                'classification' => 'College',
                 'is_active' => true,
             ],
         ];
