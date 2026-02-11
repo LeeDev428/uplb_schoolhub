d271467a50b3b911e320aa81f565d44abefd4f87
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-11 11:26:59 +0800

    Update Schedule.php

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'department_id',
+        'program_id',
+        'year_level_id',
+        'section_id',
+        'file_path',
+        'file_name',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function program()
+    {
+        return $this->belongsTo(Program::class);
+    }
+
+    public function yearLevel()
+    {
+        return $this->belongsTo(YearLevel::class);
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
 }
