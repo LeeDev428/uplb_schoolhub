9999c0994edf6dce5c0dfedf460cc1af3def6cf7
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-12 16:05:33 +0800

    Create index.ts

---
+++ resources/js/components/settings/index.ts
@@ -0,0 +1,4 @@
+export { default as ProfileForm } from './profile-form';
+export { default as PasswordForm } from './password-form';
+export { default as TwoFactorForm } from './two-factor-form';
+export { default as AppearanceForm } from './appearance-form';
