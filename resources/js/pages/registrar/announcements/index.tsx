0f61561e4aec506bb7b1b287ea28a220f5c1c388
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-13 18:00:30 +0800

    Update index.tsx

--- resources/js/pages/registrar/announcements/index.tsx
+++ resources/js/pages/registrar/announcements/index.tsx
@@ -1,6 +1,6 @@
 import AnnouncementsIndex from '@/pages/announcements/index';
 import RegistrarLayout from '@/layouts/registrar/registrar-layout';
 
-AnnouncementsIndex.layout = (page: React.ReactElement) => <RegistrarLayout>{page}</RegistrarLayout>;
+(AnnouncementsIndex as any).layout = (page: React.ReactElement) => <RegistrarLayout>{page}</RegistrarLayout>;
 
 export default AnnouncementsIndex;
