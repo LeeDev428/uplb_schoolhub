a7d361dd3eb2f17d56fa47372162fe168332d38f
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-13 18:00:32 +0800

    Update index.tsx

--- resources/js/pages/student/announcements/index.tsx
+++ resources/js/pages/student/announcements/index.tsx
@@ -1,6 +1,6 @@
 import AnnouncementsIndex from '@/pages/announcements/index';
 import StudentLayout from '@/layouts/student/student-layout';
 
-AnnouncementsIndex.layout = (page: React.ReactElement) => <StudentLayout>{page}</StudentLayout>;
+(AnnouncementsIndex as any).layout = (page: React.ReactElement) => <StudentLayout>{page}</StudentLayout>;
 
 export default AnnouncementsIndex;
