d2af7783168e46b64779bf8280ed2384bc0142a0
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-13 18:00:28 +0800

    Update index.tsx

--- resources/js/pages/parent/announcements/index.tsx
+++ resources/js/pages/parent/announcements/index.tsx
@@ -1,6 +1,6 @@
 import AnnouncementsIndex from '@/pages/announcements/index';
 import ParentLayout from '@/layouts/parent/parent-layout';
 
-AnnouncementsIndex.layout = (page: React.ReactElement) => <ParentLayout>{page}</ParentLayout>;
+(AnnouncementsIndex as any).layout = (page: React.ReactElement) => <ParentLayout>{page}</ParentLayout>;
 
 export default AnnouncementsIndex;
