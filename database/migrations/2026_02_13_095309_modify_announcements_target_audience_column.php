85797760ba60fc0e57387af80781f0c1e6cb1536
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-13 17:54:01 +0800

    Update 2026_02_13_095309_modify_announcements_target_audience_column.php

--- database/migrations/2026_02_13_095309_modify_announcements_target_audience_column.php
+++ database/migrations/2026_02_13_095309_modify_announcements_target_audience_column.php
@@ -2,16 +2,22 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
     /**
      * Run the migrations.
+     * 
+     * Change target_audience from ENUM to VARCHAR to support 'custom' and other values.
+     * Since we now use target_roles JSON column for actual targeting, this column
+     * is just for backward compatibility and general categorization.
      */
     public function up(): void
     {
-        //
+        // Change ENUM to VARCHAR(50) to allow any value including 'custom'
+        DB::statement("ALTER TABLE announcements MODIFY COLUMN target_audience VARCHAR(50) DEFAULT 'all'");
     }
 
     /**
@@ -19,6 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        // Revert to original ENUM (note: this may fail if data contains values not in the enum)
+        DB::statement("ALTER TABLE announcements MODIFY COLUMN target_audience ENUM('all', 'students', 'teachers', 'parents', 'staff') DEFAULT 'all'");
     }
 };
