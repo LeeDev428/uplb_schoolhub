bd8147d218971a67b49f7bea394877db64fc83cd
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-13 18:00:23 +0800

    Update index.tsx

--- resources/js/pages/clinic/announcements/index.tsx
+++ resources/js/pages/clinic/announcements/index.tsx
@@ -1,6 +1,6 @@
 import AnnouncementsIndex from '@/pages/announcements/index';
 import ClinicLayout from '@/layouts/clinic/clinic-layout';
 
-AnnouncementsIndex.layout = (page: React.ReactElement) => <ClinicLayout>{page}</ClinicLayout>;
+(AnnouncementsIndex as any).layout = (page: React.ReactElement) => <ClinicLayout>{page}</ClinicLayout>;
 
 export default AnnouncementsIndex;
