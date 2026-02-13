98d8e3254c40e62daac207662d0bd8d1cd4f4c5c
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-13 17:55:38 +0800

    Create index.tsx

---
+++ resources/js/pages/guidance/announcements/index.tsx
@@ -0,0 +1,6 @@
+import AnnouncementsIndex from '@/pages/announcements/index';
+import GuidanceLayout from '@/layouts/guidance/guidance-layout';
+
+AnnouncementsIndex.layout = (page: React.ReactElement) => <GuidanceLayout>{page}</GuidanceLayout>;
+
+export default AnnouncementsIndex;
