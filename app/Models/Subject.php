f4ac6216a65329575206b62abc1f73c03cbf6465
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-24 22:51:15 +0800

    Update Subject.php

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subject extends Model
 {
@@ -78,6 +79,14 @@ class Subject extends Model
     public function teachers(): BelongsToMany
     {
         return $this->belongsToMany(Teacher::class, 'subject_teacher');
+    }
+
+    /**
+     * Get student enrollment records for this subject.
+     */
+    public function studentSubjects(): HasMany
+    {
+        return $this->hasMany(StudentSubject::class);
     }
 
     /**
