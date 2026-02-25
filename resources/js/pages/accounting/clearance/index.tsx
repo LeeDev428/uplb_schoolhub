9a9a025618b449a58ef48d6c332879a73b681b79
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-26 00:29:37 +0800

    Update index.tsx

--- resources/js/pages/accounting/clearance/index.tsx
+++ resources/js/pages/accounting/clearance/index.tsx
@@ -408,7 +408,7 @@ export default function ClearanceIndex({ students, programs, yearLevels, departm
                             <TableBody>
                                 {students.data.length === 0 ? (
                                     <TableRow>
-                                        <TableCell colSpan={8} className="text-center py-8 text-gray-500">
+                                        <TableCell colSpan={9} className="text-center py-8 text-gray-500">
                                             No students found.
                                         </TableCell>
                                     </TableRow>
