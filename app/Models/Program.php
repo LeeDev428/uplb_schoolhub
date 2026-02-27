26e2d2b32714f4cf899a972a1afbc4256307d421
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-28 04:25:40 +0800

    Update Program.php

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Program extends Model
 {
@@ -29,5 +30,15 @@ class Program extends Model
     public function sections(): HasMany
     {
         return $this->hasMany(Section::class);
+    }
+
+    public function yearLevels(): HasMany
+    {
+        return $this->hasMany(YearLevel::class);
+    }
+
+    public function students(): HasManyThrough
+    {
+        return $this->hasManyThrough(Student::class, Section::class);
     }
 }
