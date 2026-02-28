6f4b1bf92fa900b1032d6a5bcfdc6d54eb8f6b76
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-01 04:33:20 +0800

    Update archived.tsx

--- resources/js/pages/registrar/archived.tsx
+++ resources/js/pages/registrar/archived.tsx
@@ -211,7 +211,7 @@ export default function ArchivedStudentsIndex({ students, filters, schoolYears, 
         <RegistrarLayout breadcrumbs={breadcrumbs}>
             <Head title="Archived Students" />
 
-            <div className="space-y-6">
+            <div className="space-y-6 p-4 md:p-6">
                 {/* Header */}
                 <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
                     <div>
