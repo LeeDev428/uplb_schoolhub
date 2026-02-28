c9c5f26df12625bbdb40eb8645b10b5957fccbe6
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-01 04:11:35 +0800

    Update accounting-layout.tsx

--- resources/js/layouts/accounting-layout.tsx
+++ resources/js/layouts/accounting-layout.tsx
@@ -1,11 +1,13 @@
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { AccountingSidebar } from '@/components/accounting/accounting-sidebar';
+import { FlashMessages } from '@/components/flash-messages';
 
 export default function AccountingLayout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AccountingSidebar />
             <SidebarInset>{children}</SidebarInset>
+            <FlashMessages />
         </SidebarProvider>
     );
 }
