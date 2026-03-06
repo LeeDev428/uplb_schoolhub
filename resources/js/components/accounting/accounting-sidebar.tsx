ba0359ea576a87b58150f03367abb2eb4bd6faf8
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-07 00:39:04 +0800

    Update accounting-sidebar.tsx

--- resources/js/components/accounting/accounting-sidebar.tsx
+++ resources/js/components/accounting/accounting-sidebar.tsx
@@ -150,7 +150,7 @@ export function AccountingSidebar() {
                                     <img src={logoUrl} alt={appName} className="size-8 rounded-lg object-contain bg-white" />
                                 ) : (
                                     <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                                        <BadgeDollarSign className="size-4" />
+                                        <PhilippinePeso className="size-4" />
                                     </div>
                                 )}
                                 <div className="grid flex-1 text-left text-sm leading-tight">
