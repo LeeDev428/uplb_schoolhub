c01304f11d5b5a240443f02954508ecaa86c3e91
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-13 17:06:17 +0800

    Create AuditReportsController.php

---
+++ app/Http/Controllers/Owner/AuditReportsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Accounting\ReportsController as AccountingReportsController;
+
+class AuditReportsController extends AccountingReportsController
+{
+    protected function reportsView(): string
+    {
+        return 'owner/audit-reports';
+    }
+}
